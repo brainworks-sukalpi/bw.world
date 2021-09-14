c6e3d58d6c96a53741c073c699088181ff52d271
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-09-14 19:19:06 +0530

    footer

---
+++ assets/js/newletter.js
@@ -0,0 +1,27 @@
+const subscribe = async () => {
+  // event.preventDefault();
+  let curTime = new Date();
+  let day = curTime.getDay();
+  let month = curTime.getMonth();
+  let year = curTime.getFullYear();
+  let minute = curTime.getMinutes();
+  let hour = curTime.getHours();
+  let time = "" + day + "-" + month + "-" + year + "  " + hour + ":" + minute;
+  console.log(time);
+  let email = document.getElementsByName("email")[0].value;
+  console.log(email);
+  let body = {
+    date: time,
+    email: email,
+  };
+  console.log(body);
+
+  // export const subscribe = async () => {
+  try {
+    const response = await axios.post("http://localhost:3000/newsletter", body);
+    const msg = response.data;
+    console.log(msg);
+  } catch (errors) {
+    console.error(errors);
+  }
+};
