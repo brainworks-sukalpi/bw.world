6b173aad06d7f34d7ce188c742fad6c49186125a
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-09-23 17:32:44 +0530

    linkedin

--- assets/js/linkedin.js
+++ assets/js/linkedin.js
@@ -7,11 +7,26 @@ const getAllPost = async () => {
     );
     ids = response.data;
     console.log(ids);
-    var iframe = document.createElement("iframe");
-    iframe.height = "500";
-    iframe.width = "70%";
-    iframe.src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${ids[0]}`;
-    document.getElementById("linkedin-post").appendChild(iframe);
+
+    ids.forEach((id) => {
+      var div = document.createElement("div");
+      if (idx == 0) {
+        div.className = "carousel-item active";
+        idx++;
+      } else {
+        div.className = "carousel-item";
+      }
+      var innerDiv = document.createElement("div");
+      innerDiv.className =
+        "w-100 col-xl-6 col-lg-6 d-flex justify-content-center align-items-center";
+      var iframe = document.createElement("iframe");
+      iframe.height = "450";
+      iframe.width = "75%";
+      iframe.src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${id}`;
+      innerDiv.appendChild(iframe);
+      div.appendChild(innerDiv);
+      document.getElementsByClassName("carousel-inner")[0].appendChild(div);
+    });
     // alert(msg);
   } catch (errors) {
     console.error(errors);
@@ -33,4 +48,8 @@ const prev = () => {
   }
 };
 getAllPost();
-next();
+// next();
+// var myCarousel = document.querySelector("#carouselExampleIndicators");
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//   cycle: "pause",
+// });
