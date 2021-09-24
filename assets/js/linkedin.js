e9d69bc9e9a54ec01551ffdf6389881e08feed4f
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-09-24 07:48:35 +0530

    linkedin loader

--- assets/js/linkedin.js
+++ assets/js/linkedin.js
@@ -1,13 +1,26 @@
 let idx = 0;
 let ids = [];
 const getAllPost = async () => {
+  //   <div class="d-flex justify-content-center">
+  //   <div class="spinner-border" role="status">
+  //     <span class="visually-hidden">Loading...</span>
+  //   </div>
+  // </div>
+  var loader = document.createElement("div");
+  loader.className = "d-flex justify-content-center";
+  var innerLoader = document.createElement("div");
+  innerLoader.className = "spinner-border loader";
+  loader.appendChild(innerLoader);
+  document.getElementsByClassName("carousel-inner")[0].appendChild(loader);
+
   try {
     const response = await axios.get(
       "https://brainworks-email-subscibers.herokuapp.com/post"
     );
     ids = response.data;
     console.log(ids);
-
+    var rem = document.getElementsByClassName("carousel-inner")[0];
+    rem.removeChild(rem.childNodes[0]);
     ids.forEach((id) => {
       var div = document.createElement("div");
       if (idx == 0) {
@@ -33,20 +46,20 @@ const getAllPost = async () => {
     alert("something went wrong!");
   }
 };
-const next = () => {
-  if (idx + 1 < ids.length) {
-    ++idx;
-    let src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${ids[idx]}`;
-    document.getElementById("linkedin-post").firstElementChild.src = src;
-  }
-};
-const prev = () => {
-  if (idx - 1 >= 0) {
-    --idx;
-    let src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${ids[idx]}`;
-    document.getElementById("linkedin-post").firstElementChild.src = src;
-  }
-};
+// const next = () => {
+//   if (idx + 1 < ids.length) {
+//     ++idx;
+//     let src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${ids[idx]}`;
+//     document.getElementById("linkedin-post").firstElementChild.src = src;
+//   }
+// };
+// const prev = () => {
+//   if (idx - 1 >= 0) {
+//     --idx;
+//     let src = `https://www.linkedin.com/embed/feed/update/urn:li:share:${ids[idx]}`;
+//     document.getElementById("linkedin-post").firstElementChild.src = src;
+//   }
+// };
 getAllPost();
 // next();
 // var myCarousel = document.querySelector("#carouselExampleIndicators");
