4a6a1d50ab13b9208d70d369aca9d582ec34e553
JB Rayansh <cooldudejbrayansh@gmail.com>
2021-10-30 15:29:33 +0530

    about

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,6 +277,39 @@
     }
   });
 
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".home-portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+        filter: ".filter-branding",
+      });
+
+      let portfolioFilters = select("#portfolio-flters li", true);
+
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
+    }
+  });
+
   /**
    * Initiate portfolio lightbox
    */
